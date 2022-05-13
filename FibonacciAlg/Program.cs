c4e01709d4f5bab5f44b79c9ddab9beb71471b05
Dtc-Xtreme <dtc-xtreme@hotmail.com>
2022-05-13 13:00:29 +0200

    Add project files.

---
+++ FibonacciAlg/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace FibonacciAlg // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(int x in GetFibonacciList())
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        static List<int> GetFibonacciList()
+        {
+            List<int> list = new List<int>();
+            int a = 0;
+            int b = 1;
+            bool forRun = false;
+
+            // Add 0.
+            list.Add(a);
+
+            if (forRun)
+            {
+                // Runs x amount of times.
+                int runCounts = 10;
+                for (int i = 0; i < runCounts - 1; i++)      // -1 run because 0 is already added.
+                {
+                    list.Add(CalcNext(ref a, ref b));
+                }
+            }
+            else
+            {
+                // Runs till the value is smaller then x.
+                int x = 10946;
+                while (list[list.Count-1] < x)
+                {
+                    list.Add(CalcNext(ref a, ref b));
+                }
+            }
+
+            return list;
+        }
+
+        static int CalcNext(ref int a, ref int b)
+        {
+            int result = a + b;
+            a = b;
+            b = result;
+            return result;
+        }
+    }
+}
